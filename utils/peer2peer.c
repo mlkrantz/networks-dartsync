3f129e39cacbb1df7132b11644a3ab1cf53e3a17
Matt Krantz <mlkrantz@gmail.com>
2015-05-31 02:16:14 -0400

    Errors are now just warning for decompression

--- utils/peer2peer.c
+++ utils/peer2peer.c
@@ -432,7 +432,7 @@ void* download_handler(void* arg) {
             char *decompressed = decompress_stream(compressed_buffer, &compressed_recv_length, &destlen);
             if (!decompressed) {
                 printf("Error decompressing file!\n");
-                fflush(stdout);
+                free(decompressed);
                 download_length = fileLen;
                 peer_flag[peer_info->idx_of_this_peer] = 2;
                 return NULL;
@@ -531,6 +531,7 @@ void* upload_handler(void* arg) {
             char *compressed = compress_stream(buffer, &file_block_length, &compressed_length);
             if (!compressed) {
                 printf("Error compressing file %s\n", message->filename);
+                free(compressed);
                 break;
             }
             // printf("Sending compressed piece of length %lu\n", compressed_length);
